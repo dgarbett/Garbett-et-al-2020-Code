5352b513eb9c1761b70d12aa95d93f4d964032d4
abisaria <abisaria@stanford.edu>
2020-05-15 00:38:59 -0700

    Commented Code

--- Ladder Assay Analysis/PRIMO_tracking.m
+++ Ladder Assay Analysis/PRIMO_tracking.m
@@ -80,9 +80,6 @@ close all
 %% Execute Code
 %
 
-
-
-
 %read in the segmentation Channel
 for ii = 1:length(segmentationCh)
     files = strcat('*',prefix, '*C',num2str(segmentationCh(1)),'*.tif');
@@ -201,12 +198,12 @@ for I = 1:numFiles
             %write the masks
             extractmemmask = bwmorph(mem_label,'remove');
             %mem_label_ring = mem_label - imerode(mem_label,strel('disk',1,0));
-            %write output files after bg subtraction
             
+            %write output files of various masks used later calculations 
             if f == 1
-                imwrite(uint16(mem_label),[RGBdir,filename,'_memedge.tif']);
-                imwrite(uint16(stripe_label),[RGBdir,filename,'_stripeedge.tif']);
-                imwrite(uint16(stripe_mask),[RGBdir,filename,'_striperaw.tif']);
+                imwrite(uint16(mem_label),[RGBdir,filename,'_memedge.tif']); %outline of membranes labeled to match memtracedata
+                imwrite(uint16(stripe_label),[RGBdir,filename,'_stripeedge.tif']); %imputed stripe outlines
+                imwrite(uint16(stripe_mask),[RGBdir,filename,'_striperaw.tif']); %raw stripe outlines before the dilation
             else
                 imwrite(uint16(mem_label),[RGBdir,filename,'_memedge.tif'],'WriteMode','append');
                 imwrite(uint16(stripe_label),[RGBdir,filename,'_stripeedge.tif'],'WriteMode','append');
@@ -220,12 +217,12 @@ for I = 1:numFiles
             
             %get centroid and area information for membranes
             memcurdata = memcurdata(memid,:);
-            mem_center = memcurdata(:,[1 2]);
-            mem_area = memcurdata(:,3);
-            mem_mass = memcurdata(:,4);
-            mem_info = regionprops(mem_label,'PixelIdxList');
+            mem_center = memcurdata(:,[1 2]); %center coordinates
+            mem_area = memcurdata(:,3); %areas in px
+            mem_mass = memcurdata(:,4); %total fluoresence 
+            mem_info = regionprops(mem_label,'PixelIdxList'); 
             %mem_raw1 = regionprops(mem_label,imMem_raw,'PixelValues');
-            mem_raw2 = regionprops(mem_label,rawMem,'PixelValues');
+            mem_raw2 = regionprops(mem_label,rawMem,'PixelValues'); %get pixel values within cell masks
             mem_nuc_num =[];
             
             memIDvec = {mem_info.PixelIdxList};
@@ -234,17 +231,16 @@ for I = 1:numFiles
             mem_info = mem_info(memidvec);
             %mem_raw1 = mem_raw1(memidvec);
             mem_raw2 = mem_raw2(memidvec);
-            %need to make sure only iterate through mem_info if it isn't
-            %empty
+            %need to make sure only iterate through mem_info if it isn't empty
             if size(mem_center,1) == 1 && ~isempty(mem_raw2)
-                mem_nuc_num(1,1) = mode(mem_raw2.PixelValues(mem_raw2.PixelValues>0));
+                mem_nuc_num(1,1) = mode(mem_raw2.PixelValues(mem_raw2.PixelValues>0)); %what number has been assigned to the membrane
             elseif ~isempty(mem_raw2)
                 for m = 1:size(mem_center,1)
                     mem_nuc_num(m,1) = mode(mem_raw2(m).PixelValues(mem_raw2(m).PixelValues>0));
                 end
             end;
             
-            %add back to memtrace data
+            %add back to memtrace data array
             memtracedata(memid,f,:) = [mem_center,mem_area,mem_mass,mem_nuc_num];
             if maxmemnum - max(memid) < memblocksize
                 memtempdata = NaN(memblocksize,endFrame,5);
@@ -255,7 +251,7 @@ for I = 1:numFiles
             end
         end;
         
-
+        %link up all the membranes 
         memtracedata(:,:,size(memtracedata,3)+1) = repmat([1:size(memtracedata,1)]',1,size(memtracedata,2));
         [memtracedata,~,~] = postprocessing_nolinking(memtracedata,memid,jitters,badframes,memtracking,maxmemnum,nucr);
         meminfo = imfinfo([RGBdir,filename,'_memedge.tif']);
@@ -267,10 +263,13 @@ for I = 1:numFiles
     close all;
 end;
 %%
+
+%%%%%%%
+%Section 2: individual cell analyses 
+
+%read in all memtracedata files
 files = dir('memtracedata*.mat');
-% if runTracking == 0 %if you don't rerun tracking load the tracedata file
-%     load([outputdir,'memtracedata_',filename,'.mat']);
-% end
+
 for f = 1:numel(files)
     load(files(f).name);
     filename = regexprep(files(f).name,'memtracedata_','');
@@ -281,13 +280,12 @@ for f = 1:numel(files)
         cellNo = cellNumber(m);
         saveFile = 1; %switch in case a cell sucks and we don't want to output the data
         meminfo = imfinfo([RGBdir,filename,'_memedge.tif']);% load tracking tiff
-        stripeinfo = imfinfo([RGBdir,filename,'_stripeedge.tif']);
-        stripeoutlineinfo = imfinfo([RGBdir,filename,'_striperaw.tif']);
+        stripeinfo = imfinfo([RGBdir,filename,'_stripeedge.tif']); % load stripe edge tiff
+        stripeoutlineinfo = imfinfo([RGBdir,filename,'_striperaw.tif']); % load  raw stripe tracking tiff
         cellData=[];
         
         
         cellData = squeeze(memtracedata(m,:,:));
-        %disp(['cell = ',num2str(m)]);
         
         %see that the cell was tracked
         longestrun = regionprops(~isnan(cellData(:,1)),'Area','PixelIdxList');
@@ -312,10 +310,11 @@ for f = 1:numel(files)
         end
         
         frames_tracked = idx_first:idx_last;
-        
-        
-         %calculate MSD from the first position
+
+         %calculate Mean Squared Displaceent from the first position
         MSD = pdist2([cellData(idx_first,1:2)],[cellData(idx_first:idx_last,1:2)])*umPerPx;
+        
+        %start looking at contact with stripes based on the # of tracked frames
         allStripes = [];
         stripe = {};
         numStripes  = nan(size(cellData,1),1);
@@ -323,36 +322,37 @@ for f = 1:numel(files)
         for frames=1:length(frames_tracked) %iterate through tracked frames;
             frame = frames_tracked(frames);
             memEdgeImage=imread([RGBdir,filename,'_memedge.tif'],frame, 'Info', meminfo);
-            memEdgeImageCell=uint16(memEdgeImage== cellNo) ;
+            memEdgeImageCell=uint16(memEdgeImage== cellNo); %only take segmented area of cell
             
             stripeImage = imread([RGBdir,filename,'_stripeedge.tif'],frame, 'Info', stripeinfo);
-            uniqueStripes = stripeImage.*(memEdgeImageCell);
+            uniqueStripes = stripeImage.*(memEdgeImageCell); %which stripes have overlap with membrane
             whichStripes = unique(uniqueStripes);
             whichStripes = whichStripes(2:end);
-            stripe{frame} = whichStripes;
-            allStripes = unique([allStripes ; whichStripes]);
-            numStripes(frame) = length(whichStripes);
+            stripe{frame} = whichStripes; %which stripes were occupied at this time
+            allStripes = unique([allStripes ; whichStripes]);  %which stipes have ever been occupied
+            numStripes(frame) = length(whichStripes); %how many stripes were occupied
             
             actualStripe = imread([RGBdir,filename,'_striperaw.tif'],frame, 'Info', stripeoutlineinfo);
             overlap = uint16(actualStripe>0) .* memEdgeImageCell;
-            adhesionArea(frame) = sum(overlap(:));
+            adhesionArea(frame) = sum(overlap(:)); %how much overlap between stripes and cell = adhesion area
         end
         
+        %code to automatically detect which stripe the cells were located on
         stripeImage = imread([RGBdir,filename,'_stripeedge.tif'],idx_last, 'Info', stripeinfo);
         StripeInfo = regionprops(stripeImage, 'Centroid');
         stripeCenters = cell2mat(struct2cell(StripeInfo)');
-        occupiedCenters = stripeCenters([whichStripes],:);
+        occupiedCenters = stripeCenters([whichStripes],:); 
         if size(occupiedCenters,1) > 1
-            trackGap = mean(diff(occupiedCenters(:,1))*.325);
+            trackGap = mean(diff(occupiedCenters(:,1))*.325); %what is the averge distance between the occupied stripes
         else
             trackGap = 0;
         end;
         
         %assign the stripe
         if numLanes > 1
-            clusterLanes = kmeans(stripeCenters(:,2),numLanes);
+            clusterLanes = kmeans(stripeCenters(:,2),numLanes); %cluster stripes based on the vertical axis
             tbl = tabulate(clusterLanes);
-            rowOrder = sortrows(tbl,2,'descend');
+            rowOrder = sortrows(tbl,2,'descend'); %the closest together gaps will have te highest number of stripes
             rowOrder = [rowOrder [1:numLanes]'];
             rowOrder = sortrows(rowOrder,1,'ascend');
             for l = 1:length(clusterLanes)
@@ -362,11 +362,11 @@ for f = 1:numel(files)
             
             cellLane = mode(LaneAssigned([whichStripes]));
         else
-            cellLane = 1;
+            cellLane = 1; %if there are not different lane widths
         end
         
         
-        
+        %save ak te cell data
         cellData = [cellData numStripes adhesionArea];
         save([outputdir,filename,'_cell_analysis_',num2str(cellNo),'.mat'],'cellData','cellLane','trackGap','stripe','allStripes');
         
@@ -378,24 +378,25 @@ end
 %combine all data
 
 parentdir = '/Volumes/AB_Data_3/DG_tplastin/20190920_BJ5 CRISPR analysis';
-minutesTracked = 60;
-subdirlist = {'WT' 'KO'};
-trackingName = 'Tracking_V1';
+
+minutesTracked = 60; %how long to check for tracking to keep a cell.  keep this the same to compare between replciates/conditions
+subdirlist = {'WT' 'KO'}; %subdirs to look for cell data
+trackingName = 'Tracking_V1'; 
 cd(parentdir);
 folderlist = dir(parentdir);
 SummaryTable = {};
 counter = 0;
-timeStep = .5;
+timeStep = .5; %how long between imaging
 umperpx = .325;
 for s = 1:numel(subdirlist)
    % try
         disp(['Condition is ' subdirlist{s}]);
         cd([parentdir  '/' subdirlist{s} '/' trackingName]);
-        allCellAnalysis = dir('*cell_analysis*.mat');
+        allCellAnalysis = dir('*cell_analysis*.mat'); %read all cell data
         for a = 1:numel(allCellAnalysis)
             load(allCellAnalysis(a).name)
                 counter = counter+1;
-                longestrun = regionprops(~isnan(cellData(:,1)),'Area','PixelIdxList');
+                longestrun = regionprops(~isnan(cellData(:,1)),'Area','PixelIdxList'); %look through longest tracked 
                 if isempty(longestrun)
                     continue;
                 end
@@ -403,17 +404,15 @@ for s = 1:numel(subdirlist)
                 idx_first = longestrun(sortIndexes(1)).PixelIdxList(1);
                 idx_last = longestrun(sortIndexes(1)).PixelIdxList(end);
                 
-                %WT or mutant?
+                %WT or mutant? (based on subdirlist)
                 SummaryTable{counter,1} = s;
-                %which exp did it come from
+                %which exp did it come from 
                 SummaryTable{counter,2} = allCellAnalysis(a).name;
                 
-                
-                %position
-                
-                
+                  
                 [~, endIndex] = regexp(allCellAnalysis(a).name,'cell_analysis_');
                 [startIndex,~] = regexp(allCellAnalysis(a).name,'.mat');
+                %get the cell number
                 cellNumber = extractBetween(allCellAnalysis(a).name,endIndex+1, startIndex-1);
                 
                 %cellnumber
@@ -438,17 +437,10 @@ for s = 1:numel(subdirlist)
                 %starting cell area
                 SummaryTable{counter,5} = cellData(stripe_first,3)*umperpx^2;
                 
-                
-                %does it increase at least one stripe?
-                
                 timeTracked = stripe_last-stripe_first;
                 SummaryTable{counter,6} =timeTracked;
-%                 if isempty(timeTracked) 
-%                     SummaryTable{counter,6} = nan;
-%                 end
                 numhrs = (timeTracked*timeStep);
-                %SummaryTable{counter,12} = numhrs;
-                
+               
                 firstTimeVar = 6;
                 if (timeTracked*timeStep) >= minutesTracked
                     %dArea after 1 hour
@@ -465,11 +457,13 @@ for s = 1:numel(subdirlist)
 %end % through conditions
 %end % throught folders
 
+%Summary table variables: [1:subdir(#) 2:filename 3:cell(#) 4:cell lane 5:starting area in um2 6:time tracked (frames) 7:area change after 1 hour 8:change in bars after 1 hour
+
 %%
 for i = 1:size(SummaryTable,1)
     for j = 1 :size(SummaryTable,2)
         if isempty(SummaryTable{i,j})
-            SummaryTable{i,j} = NaN;
+            SummaryTable{i,j} = NaN; %fil empty values with nan
         end
         
         
@@ -480,14 +474,13 @@ end
 TableAll = (cell2table(SummaryTable));
 varnames = {'Cell_Line','Cell_Folder','Cell_Number','Cell_Lane','Start_Area','tracked_time','hrdArea','hrdStripes'};
 TableAll.Properties.VariableNames = varnames;
-
+%%
 %what are the cells you want to analyze
-singleCell = cell2mat(SummaryTable(:,5)) < 5000;
-%singlePost =   cell2mat(SummaryTable(:,8)) < 3;
+singleCell = cell2mat(SummaryTable(:,5)) < 5000; %initial tracked area needs to be smaller than this
+%singlePost =   cell2mat(SummaryTable(:,8)) < 3; %can set to make sure only sat down on a single post 
 Tracked = cell2mat(SummaryTable(:,6)) > minutesTracked/timeStep;
-celltype = cell2mat(SummaryTable(:,1)) ==  1;
+celltype = cell2mat(SummaryTable(:,1)) ==  1 %set depending on what subdir you want to analuze
 ForAnalysis = TableAll(singleCell&Tracked&celltype,:);
-
 
 allVals = NaN(1,size(ForAnalysis,1),size(ForAnalysis,2) -4);
 
