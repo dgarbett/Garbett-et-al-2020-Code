6810efa28c3a302721396c57e70f123a6a5b4c33
abisaria <abisaria@stanford.edu>
2020-05-15 00:48:51 -0700

    Commented code for Garbett et al., 2020

--- Ladder Assay Analysis/getAreaChange.m
+++ Ladder Assay Analysis/getAreaChange.m
@@ -1,3 +1,6 @@
+%code for tracking the average area change of cells over time from when they first landed on a stripe
+
+%information for which files to look for
 parentdir = '/Volumes/AB_Data_3/DG_tplastin/1_9 area replot';
 %filesdir = 'New Controls';
 minutesTracked = 60;
@@ -10,32 +13,29 @@ counter = 0;
 timeStep = .5;
 umperpx = .325;
 counter2 = 0;
-for f= 4
+for f= 1:length(folderlist)
     cd(parentdir);
     cd(folderlist(f).name);
 
         try
-
             allCellAnalysis = dir('*cell_analysis*.mat');
             for a = 1:numel(allCellAnalysis)
                 load( allCellAnalysis(a).name)
-                if isnan(cellLane)
+                if isnan(cellLane) %must have been tracked on a lane
                     continue
                 else
                     counter2 = counter2 + 1;
                     
                     longestrun = regionprops(~isnan(cellData(:,1)),'Area','PixelIdxList');
                     if isempty(longestrun)
-                        continue;
+                        continue; %must have been tracked
                     end
                     [sortedAreas, sortIndexes] = sort([longestrun.Area], 'Descend');
                     idx_first = longestrun(sortIndexes(1)).PixelIdxList(1);
                     idx_last = longestrun(sortIndexes(1)).PixelIdxList(end);
                     
-                    
                     %when is it on a stripe?
-                    longeststripe = regionprops(~isnan(cellData(:,3)) & cellData(:,7)>0 ,'Area','PixelIdxList');
-                    
+                    longeststripe = regionprops(~isnan(cellData(:,3)) & cellData(:,7)>0 ,'Area','PixelIdxList'); 
                     if isempty(longeststripe)
                         continue;
                     end
@@ -51,24 +51,20 @@ for f= 4
                     if cellData(stripe_first,3)*umperpx^2 > 5000
                         continue;
                     end
-                    %SummaryTable{counter,5} = cellData(stripe_first,3)*umperpx^2;
-                    
+                  
                     
                     timeTracked = stripe_last-stripe_first;
-                    
                     numhrs = (timeTracked*timeStep);
                     
-                    if (timeTracked*timeStep) >= minutesTracked
+                    if (timeTracked*timeStep) >= minutesTracked %make sure it was tracked long enough
                         counter = counter + 1;
                         numpts = minutesTracked/timeStep;
                         cellArea = cellData(:,3)*(umperpx^2);
-                        area(counter,:) = cellArea(stripe_first:stripe_first+numpts);
+                        area(counter,:) = cellArea(stripe_first:stripe_first+numpts); 
                     else
                         continue;
                     end
-                    
-                    
-                    
+                
                 end%on a known stripe
             end %through files
         end %try
